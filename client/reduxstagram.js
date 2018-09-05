985792d854411cac9ce0d2beb283d1b87352e738
Andrew Stark <andrewjohnstark@gmail.com>
2018-09-05 05:21:51 +0100

    First commit

---
+++ client/reduxstagram.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+import { render } from 'react-dom';
+
+import Main from './components/Main';
+
+// import CSS
+import css from './styles/style.styl';
+
+render(<Main />, document.getElementById('root'));
